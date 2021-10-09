918df81b0bcc177896fa1ad1c50a129f48a8c818
collinskoech11 <collinskoechck34@gmail.com>
2021-10-09 08:25:50 +0300

    update closures

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log(count);
 
 // IMmmediately Invoked Function Expression 
 (function() {
-    console.log("hello);
+    console.log("hello")
 })();
 //closures 
 let greeting = (function() {
@@ -14,7 +14,21 @@ let greeting = (function() {
     let getMessage = function() {
         return message; 
     };
-    return getMessage: getMessage, //clossure
+    return{
+         getMessage: getMessage, //clossure
+    }
 })(); 
 //console.log(greeting.message);   //undefined without closures
 console.log(greeting.getMessage());
+
+//closure eg.2
+function setupCounter( val ) {
+    return function counter() {
+        return val++;
+    }
+}
+let counter1 = setupCounter(0);
+console.log(counter1());
+console.log(counter1());
+let counter2 = setupCounter(10);
+console.log(counter2());
