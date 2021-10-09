daf3ae0a36c5384856196f1fd4c16b5cf1ce29aa
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2021-10-09 08:14:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ if (message === 'hello') {
     var count = 100;// without 'var' => reference error
 }
 console.log(count);
+
+// IMmmediately Invoked Function Expression 
+(function() {
+    console.log("hello);
+})();
+ //closures 
+let greeting = (function() {
+    let message = 'hello';
+    let getMessage = function() {
+        return message; 
+    };
+    return getMessage: getMessage, //clossure
+})(); 
+//console.log(greeting.message);   //undefined without closures
+console.log(greeting.getMessage());
